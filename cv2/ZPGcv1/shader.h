cf77d8ba683d15676c27edffc65c808b648129cc
Kseniia Blokhina <ksublokhina@gmail.com>
2024-10-04 00:36:04 +0200

    r

--- cv2/ZPGcv1/shader.h
+++
@@ -1,8 +0,0 @@
-#include <GL/glew.h>
-
-GLuint create_shader_program();
-GLuint create_shader_program1();
-
-GLuint create_shader_program2();
-GLuint create_shader_program3();
-GLuint create_shader_program4();
